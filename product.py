2be7a436cde74256987ef3c8561f36e4d5357edc
Qing Sun <sunqingshine@163.com>
2020-02-11 13:17:24 +0100

    写入文档

--- product.py
+++ product.py
@@ -12,4 +12,13 @@ print(products)
 
 for p in products:
 	print(p[0], 'its price is', p[1])
-	
+
+with open('products.txt', 'w') as f: 
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')  
+# w是写入模式，所以products文档存不存在不重要，有则覆盖无则新建
+# open只是打开档案
+#字串可以直接做加法和乘法运算
+#f.write才是写入动作，\n是换行的意思
+
+
